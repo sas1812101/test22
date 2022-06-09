48fec69c0d693b429e9a69a54963b0555b0cf480
sas1812101 <104480732+sas1812101@users.noreply.github.com>
2022-06-09 11:46:48 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,33 @@
 basic.forever(function () {
-	
+    serial.writeLine("" + (sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    )))
+    basic.showNumber(sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    ))
+    if (sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    ) > 1) {
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            . # # # .
+            . # . # .
+            . # . # .
+            `)
+    } else {
+        basic.showLeds(`
+            # # # . .
+            # . . # .
+            # # # . .
+            # . . # .
+            # # # . .
+            `)
+    }
 })
